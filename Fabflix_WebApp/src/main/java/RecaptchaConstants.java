c5418f0a0939ff337ace9913c298bb009c4092f1
hadidip <hadidip@uci.edu>
2020-05-11 11:31:12 -0700

    Began reimplementation of HTTPS

--- Fabflix_WebApp/src/main/java/RecaptchaConstants.java
+++ Fabflix_WebApp/src/main/java/RecaptchaConstants.java
@@ -2,6 +2,6 @@ package main.java;
 
 public class RecaptchaConstants {
 
-    public static final String SECRET_KEY = "6LcYW_EUAAAAAIkY3FVWzoSobSNzBSXqIV-F_fpb";
+    public static final String SECRET_KEY = "6LeKvPAUAAAAAMTyDDWlTXzn1miYtYh5OvV6F5PZ";
 
 }
