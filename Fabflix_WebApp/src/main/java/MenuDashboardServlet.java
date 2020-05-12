e970679b916670835c77bc60e297fe5794681827
hadidip <hadidip@uci.edu>
2020-05-12 01:07:21 -0700

    Corrected table metadata using iterative loop and string array

--- Fabflix_WebApp/src/main/java/MenuDashboardServlet.java
+++ Fabflix_WebApp/src/main/java/MenuDashboardServlet.java
@@ -33,7 +33,7 @@ public class MenuDashboardServlet extends HttpServlet {
             Connection dbcon = dataSource.getConnection();
             DatabaseMetaData databaseMetaData = dbcon.getMetaData();
             ResultSet resultSet = databaseMetaData.getTables(null, null, "%", new String[]{"TABLE"});
-            ResultSet columns = databaseMetaData.getColumns(null, null, "%", null);
+
 
             while (resultSet.next()) {
                 System.out.println(resultSet.getString("movies"));
@@ -48,41 +48,43 @@ public class MenuDashboardServlet extends HttpServlet {
                 System.out.println(resultSet.getString("employees"));
             }
 
-            while(columns.next())
-            {
-                String columnName = columns.getString("COLUMN_NAME");
-                String datatype = columns.getString("DATA_TYPE");
-                String columnsize = columns.getString("COLUMN_SIZE");
-                String decimaldigits = columns.getString("DECIMAL_DIGITS");
-                String isNullable = columns.getString("IS_NULLABLE");
-                String is_autoIncrment = columns.getString("IS_AUTOINCREMENT");
-                //Printing results
-                System.out.println(columnName + "---" + datatype + "---" + columnsize + "---" + decimaldigits + "---" + isNullable + "---" + is_autoIncrment);
+            String[] tables = {"movies", "stars", "stars_in_movies", "genres", "genres_in_movies", "customers", "creditcards", "sales", "ratings", "employees"};
+
+            for (String t: tables) {
+
+                ResultSet columns = databaseMetaData.getColumns(null, null, t, null);
+
+                while (columns.next()) {
+                    String columnName = columns.getString("COLUMN_NAME");
+                    String datatype = columns.getString("DATA_TYPE");
+                    String columnsize = columns.getString("COLUMN_SIZE");
+                    String decimaldigits = columns.getString("DECIMAL_DIGITS");
+                    String isNullable = columns.getString("IS_NULLABLE");
+                    String is_autoIncrment = columns.getString("IS_AUTOINCREMENT");
+                    //Printing results
+                    System.out.println(columnName + "---" + datatype + "---" + columnsize + "---" + decimaldigits + "---" + isNullable + "---" + is_autoIncrment);
+                }
+
+                ResultSet PK = databaseMetaData.getPrimaryKeys(null, null, t);
+                System.out.println("------------PRIMARY KEYS-------------");
+                while (PK.next()) {
+                    System.out.println(PK.getString("COLUMN_NAME") + "===" + PK.getString("PK_NAME"));
+                }
+
+                ResultSet FK = databaseMetaData.getImportedKeys(null, null, t);
+                System.out.println("------------FOREIGN KEYS-------------");
+                while (FK.next()) {
+                    System.out.println(FK.getString("PKTABLE_NAME") + "---" + FK.getString("PKCOLUMN_NAME") + "===" + FK.getString("FKTABLE_NAME") + "---" + FK.getString("FKCOLUMN_NAME"));
+                }
+
+                FK.close();
+                PK.close();
+                columns.close();
+                JsonObject responseJsonObject = new JsonObject();
+
+                responseJsonObject.addProperty("status", "success");
+                responseJsonObject.addProperty("message", "success");
             }
-
-            ResultSet PK = databaseMetaData.getPrimaryKeys(null,null, "%");
-            System.out.println("------------PRIMARY KEYS-------------");
-            while(PK.next())
-            {
-                System.out.println(PK.getString("COLUMN_NAME") + "===" + PK.getString("PK_NAME"));
-            }
-
-            ResultSet FK = databaseMetaData.getImportedKeys(null, null, "%");
-            System.out.println("------------FOREIGN KEYS-------------");
-            while(FK.next())
-            {
-                System.out.println(FK.getString("PKTABLE_NAME") + "---" + FK.getString("PKCOLUMN_NAME") + "===" + FK.getString("FKTABLE_NAME") + "---" + FK.getString("FKCOLUMN_NAME"));
-            }
-
-
-            JsonObject responseJsonObject = new JsonObject();
-
-            responseJsonObject.addProperty("status", "success");
-            responseJsonObject.addProperty("message", "success");
-            
-            FK.close();
-            PK.close();
-            columns.close();
             resultSet.close();
             dbcon.close();
         } catch (Exception e) {
