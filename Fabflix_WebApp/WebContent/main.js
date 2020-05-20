263aab7206e7d2d4426c302d79920575b00a8e63
dnguyen19899 <dnguyen19899@gmail.com>
2020-05-20 05:05:43 -0700

    modified main.js autocomplete function to match more with the example, but still not working

--- Fabflix_WebApp/WebContent/main.js
+++ Fabflix_WebApp/WebContent/main.js
@@ -67,22 +67,7 @@ function handleGenreResult(resultData) {
         "</form>");
 }
 
-$('#search_bar').autocomplete({
-    // documentation of the lookup function can be found under the "Custom lookup function" section
-    lookup: function (query, doneCallback) {
-        handleLookup(query, doneCallback)
-    },
-    onSelect: function(suggestion) {
-        handleSelectSuggestion(suggestion)
-    },
-    // set delay time
-    deferRequestBy: 300,
-    // there are some other parameters that you might want to use to satisfy all the requirements
-    // TODO: add other parameters, such as minimum characters
-});
-
 function handleLookup(query, doneCallback) {
-    // TODO: merge the autocomplete example code with our existing code
     console.log("autocomplete initiated")
     console.log("sending AJAX request to backend Java Servlet")
 
@@ -94,18 +79,26 @@ function handleLookup(query, doneCallback) {
         "method": "GET",
         // generate the request url from the query.
         // escape the query string to avoid errors caused by special characters
-        "url": "/movie-suggestion",
-        "success": function (data) {
+        "url": "movie-suggestion?query=" + escape(query),
+        "success": function(data) {
             // pass the data, query, and doneCallback function into the success handler
             handleLookupAjaxSuccess(data, query, doneCallback)
         },
-        "error": function (errorData) {
+        "error": function(errorData) {
             console.log("lookup ajax error")
             console.log(errorData)
         }
     })
 }
 
+
+/*
+ * This function is used to handle the ajax success callback function.
+ * It is called by our own code upon the success of the AJAX request
+ *
+ * data is the JSON data string you get from your Java Servlet
+ *
+ */
 function handleLookupAjaxSuccess(data, query, doneCallback) {
     console.log("lookup ajax successful")
 
@@ -121,11 +114,44 @@ function handleLookupAjaxSuccess(data, query, doneCallback) {
     doneCallback( { suggestions: jsonData } );
 }
 
+
+/*
+ * This function is the select suggestion handler function.
+ * When a suggestion is selected, this function is called by the library.
+ *
+ * You can redirect to the page you want using the suggestion data.
+ */
 function handleSelectSuggestion(suggestion) {
     // TODO: jump to the specific result page based on the selected suggestion
 
-    console.log("you select " + suggestion["title"] + " with ID " + suggestion["id"])
+    console.log("you select " + suggestion["value"] + " with ID " + suggestion["data"]["heroID"])
 }
+
+
+/*
+ * This statement binds the autocomplete library with the input box element and
+ *   sets necessary parameters of the library.
+ *
+ * The library documentation can be find here:
+ *   https://github.com/devbridge/jQuery-Autocomplete
+ *   https://www.devbridge.com/sourcery/components/jquery-autocomplete/
+ *
+ */
+// $('#autocomplete') is to find element by the ID "autocomplete"
+$('#autocomplete').autocomplete({
+    // documentation of the lookup function can be found under the "Custom lookup function" section
+    lookup: function (query, doneCallback) {
+        handleLookup(query, doneCallback)
+    },
+    onSelect: function(suggestion) {
+        handleSelectSuggestion(suggestion)
+    },
+    // set delay time
+    deferRequestBy: 300,
+    // there are some other parameters that you might want to use to satisfy all the requirements
+    // TODO: add other parameters, such as minimum characters
+});
+
 
 /*
  * do normal full text search if no suggestion is selected
@@ -136,13 +162,15 @@ function handleNormalSearch(query) {
 }
 
 // bind pressing enter key to a handler function
-$('#search_body').keypress(function(event) {
+$('#autocomplete').keypress(function(event) {
     // keyCode 13 is the enter key
     if (event.keyCode == 13) {
         // pass the value of the input box to the handler function
-        handleNormalSearch($('#search_body').val())
+        handleNormalSearch($('#autocomplete').val())
     }
 })
+
+// TODO: if you have a "search" button, you may want to bind the onClick event as well of that button
 
 /**
  * Once this .js is loaded, following scripts will be executed by the browser
