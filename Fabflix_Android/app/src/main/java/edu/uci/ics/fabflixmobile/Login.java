96830542cb3d4591596d95c2e162d90d36fdb9a3
hadidip <hadidip@uci.edu>
2020-05-20 15:26:01 -0700

    added recaptcha to mobile, but seems to verify and crash at the same time

--- Fabflix_Android/app/src/main/java/edu/uci/ics/fabflixmobile/Login.java
+++ Fabflix_Android/app/src/main/java/edu/uci/ics/fabflixmobile/Login.java
@@ -1,5 +1,6 @@
 package edu.uci.ics.fabflixmobile;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,14 +8,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.safetynet.SafetyNet;
+import com.google.android.gms.safetynet.SafetyNetApi;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -28,6 +33,7 @@ public class Login extends AppCompatActivity {
     private TextView message;
     private Button loginButton;
     private String url;
+    private String SITE_KEY = "6LeKvPAUAAAAABY_fGoGUY8FVwSafDXBozZJvk4I";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,7 +60,35 @@ public class Login extends AppCompatActivity {
     }
 
     public void login() {
+        //TODO: Idantify what causes crash even when reCAPTCHA appears verified
+        SafetyNet.getClient(this).verifyWithRecaptcha(SITE_KEY)
+                .addOnSuccessListener((Activity) this,
+                        new OnSuccessListener<SafetyNetApi.RecaptchaTokenResponse>() {
+                            @Override
+                            public void onSuccess(SafetyNetApi.RecaptchaTokenResponse response) {
+                                // Indicates communication with reCAPTCHA service was
+                                // successful.
+                                String userResponseToken = response.getTokenResult();
+                                if (!userResponseToken.isEmpty()) {
+                                    // Validate the user response token using the
+                                    // reCAPTCHA siteverify API.
+                                }
+                            }
+                        })
+                .addOnFailureListener((Activity) this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        if (e instanceof ApiException) {
+                            // An error occurred when communicating with the
+                            // reCAPTCHA service. Refer to the status code to
+                            // handle the error appropriately.
+                            ApiException apiException = (ApiException) e;
+                            int statusCode = apiException.getStatusCode();
 
+                        } else {
+                        }
+                    }
+                });
         message.setText("Trying to login");
         // Use the same network queue across our application
         final RequestQueue queue = NetworkManager.sharedManager(this).queue;
