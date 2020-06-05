6b1d70a64900c2932f5b774c3df4e96f96444765
hadidip <hadidip@uci.edu>
2020-06-05 12:57:34 -0700

    Added log processor

---
+++ Fabflix_WebApp/WebContent/log_processor.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  5 03:41:45 2020
+
+@author: duy19
+"""
+
+import json
+
+
+def main():
+
+    TS = 0
+    TJ = 0
+    counter = 0
+    with open('time_log.txt') as f:
+        for line in f:
+            counter += 1
+            TS += json.loads(line)["ts"]
+            TJ += json.loads(line)["tj"]
+
+    TS = TS / counter
+    TJ = TJ / counter
+    
+    print("TS:", TS)
+    print("TJ:", TJ)
+    
+if __name__ == "__main__":
+    main()
